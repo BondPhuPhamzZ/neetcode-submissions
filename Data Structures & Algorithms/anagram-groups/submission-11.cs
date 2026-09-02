2c9abaac42e4b4acff82e88ddb1b8d75279ca375
Phạm Gia Phú <phuphamwork89@gmail.com>
2026-09-02 12:54:41 +0700

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();
+
+        foreach(string s in strs)
+        {
+            char[] count = new char[26];
+            foreach(char c in s)
+            {
+                count[c - 'a']++;
+            }
+
+            string key = new string(count);
+
+            if (!map.ContainsKey(key))
+            {
+                map[key] = new List<string>(); 
+            }
+            map[key].Add(s);
+
+        }
+
+        return new List<List<string>>(map.Values);
+
+    }
+}
