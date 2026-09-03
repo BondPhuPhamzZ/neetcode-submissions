80a37587fc92e6c15bd65aa1a7fa97269b5eade0
Phạm Gia Phú <phuphamwork89@gmail.com>
2026-09-03 20:14:55 +0700

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        Dictionary<int, int> countNum = new Dictionary<int, int>();
+        
+        int target = nums.Length / 2;
+
+        foreach(int num in nums)
+        {
+            if(!countNum.ContainsKey(num))
+            {
+                countNum[num] = 0;
+            }
+            countNum[num]++;
+
+            if (countNum[num] > target)
+            {
+                return num;
+            }
+
+        }
+
+        return 0;
+
+    }
+}
