e6f5d689093564189ce0cf0c46ca304212edc73e
Phạm Gia Phú <phuphamwork89@gmail.com>
2026-09-04 12:54:54 +0700

    Add: remove-element - submission-11

---
+++ Data Structures & Algorithms/remove-element/submission-11.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.Length;
+
+        while (left < right)
+        {
+            if (nums[left] == val)
+            {
+                nums[left] = nums[right - 1];
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+
+        return right;
+    }
+}
