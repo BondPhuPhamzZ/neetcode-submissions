b8b1d48d323d4c9135d105d137375160ef8d74db
Phạm Gia Phú <phuphamwork89@gmail.com>
2026-09-06 01:35:10 +0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+
+        int maxProfit = 0;
+
+        while (right < prices.Length)
+        {
+            if (prices[left] < prices[right])
+            {
+                int currentProfit = prices[right] - prices[left];
+                maxProfit = Math.Max(maxProfit, currentProfit);
+            }
+            else
+            {
+                left = right;
+            }
+            right++;
+        }
+        return maxProfit;
+    }
+}
