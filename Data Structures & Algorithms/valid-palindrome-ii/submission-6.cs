c5e33a50337b658aa3f697dfe41748ae02f21282
Phạm Gia Phú <phuphamwork89@gmail.com>
2026-09-11 20:48:22 +0700

    Add: valid-palindrome-ii - submission-6

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-6.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public bool ValidPalindrome(string s) {
+        int left = 0;
+        int right = s.Length - 1;
+        
+        while (left < right)
+        {
+            if (s[left] == s[right])
+            {
+                left++;
+                right--;
+            }
+            else
+            {
+                bool skipLeft = IsStrictPalindrome(s, left + 1, right);
+                bool skipRight = IsStrictPalindrome(s, left, right - 1);
+                return skipLeft || skipRight;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsStrictPalindrome(string s, int left, int right)
+    {
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+}
