f19359acabc60075b2721eb87076a3dcc514c2b8
Phạm Gia Phú <phuphamwork89@gmail.com>
2026-09-15 23:42:14 +0700

    Add: contains-duplicate-ii - submission-7

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-7.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int currentNum = nums[i];
+
+            if (dict.ContainsKey(currentNum))
+            {
+                int oldIndex = dict[currentNum];
+                int distance = i - oldIndex;
+
+                if (distance <= k)
+                {
+                    return true;
+                }
+
+                dict[currentNum] = i;
+
+            }
+            else
+            {
+                dict.Add(currentNum, i);
+            }
+        }
+
+        return false;
+
+    }
+}
